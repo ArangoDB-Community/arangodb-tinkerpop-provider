43b925d7cb190fac5c95641763d2e87bd99c20fd
a-brandt <a.brandt@triagens.de>
2013-11-01 10:45:49 +0100

    arangod version 1.4.0

--- tests/travis/setup_arangodb.sh
+++ tests/travis/setup_arangodb.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+R="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-VERSION=1.3.1
+VERSION=1.4.0
 NAME=ArangoDB-$VERSION
 
 if [ ! -d "$DIR/$NAME" ]; then
   # download ArangoDB
-  echo "wget http://www.arangodb.org/travisCI/$NAME.tar.gz"
-  wget http://www.arangodb.org/travisCI/$NAME.tar.gz
+  echo "wget http://www.arangodb.org/repositories/travisCI/$NAME.tar.gz"
+  wget http://www.arangodb.org/repositories/travisCI/$NAME.tar.gz
   echo "tar zxf $NAME.tar.gz"
   tar zvxf $NAME.tar.gz
 fi
@@ -19,28 +17,23 @@ PID=$(echo $PPID)
 TMP_DIR="/tmp/arangodb.$PID"
 PID_FILE="/tmp/arangodb.$PID.pid"
 ARANGODB_DIR="$DIR/$NAME"
-
 ARANGOD="${ARANGODB_DIR}/bin/arangod"
-if [ "$ARCH" == "x86_64" ]; then
-  ARANGOD="${ARANGOD}_x86_64"
-fi
 
 # create database directory
 mkdir ${TMP_DIR}
 
-echo "Starting arangodb '${ARANGOD}'"
+echo "Starting ArangoDB '${ARANGOD}'"
 
 ${ARANGOD} \
-    --database.directory ${TMP_DIR}  \
-    --configuration none  \
+    --database.directory ${TMP_DIR} \
+    --configuration none \
     --server.endpoint tcp://127.0.0.1:8529 \
+    --javascript.app-path ${ARANGODB_DIR}/js/apps \
     --javascript.startup-directory ${ARANGODB_DIR}/js \
-    --server.admin-directory ${ARANGODB_DIR}/html/admin \
     --javascript.modules-path ${ARANGODB_DIR}/js/server/modules:${ARANGODB_DIR}/js/common/modules:${ARANGODB_DIR}/js/node \
     --javascript.package-path ${ARANGODB_DIR}/js/npm:${ARANGODB_DIR}/js/common/test-data/modules \
-    --javascript.action-directory ${ARANGODB_DIR}/js/actions  \
-    --database.maximal-journal-size 1048576  \
-    --server.disable-admin-interface true \
+    --javascript.action-directory ${ARANGODB_DIR}/js/actions \
+    --database.maximal-journal-size 1048576 \
     --server.disable-authentication true \
     --javascript.gc-interval 1 &
 
@@ -62,3 +55,4 @@ while [[ -z `curl -s 'http://127.0.0.1:8529/_api/version' ` ]] ; do
 done
 
 echo "ArangoDB is up"
+
