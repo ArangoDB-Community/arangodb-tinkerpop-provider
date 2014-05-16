b9d99eb9913f1bc0324c32151464b6e29a0bf774
a-brandt <a.brandt@triagens.de>
2014-05-16 11:45:45 +0200

    changes for gremlin 2.5.0

--- src/main/java/com/tinkerpop/blueprints/impls/arangodb/utils/ArangoDBConfiguration.java
+++ src/main/java/com/tinkerpop/blueprints/impls/arangodb/utils/ArangoDBConfiguration.java
@@ -1,67 +1,76 @@
 package com.tinkerpop.blueprints.impls.arangodb.utils;
 
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.configuration.SubnodeConfiguration;
+
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.impls.arangodb.ArangoDBGraph;
 import com.tinkerpop.rexster.Tokens;
 import com.tinkerpop.rexster.config.GraphConfiguration;
+import com.tinkerpop.rexster.config.GraphConfigurationContext;
 import com.tinkerpop.rexster.config.GraphConfigurationException;
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.HierarchicalConfiguration;
-import org.apache.commons.configuration.SubnodeConfiguration;
 
 /**
-* Rexster configuration for ArangoDB graphs. Accepts configuration in rexster.xml as follows:
-*
-* <code>
-* <graph>
-* <graph-name>arangodb-rexter-graph</graph-name>
-* <graph-type>com.tinkerpop.blueprints.impls.arangodb.util.ArangoDBGraphConfiguration</graph-type>
-* <properties>
-* <vertex-name>arangodb-rexter-graph-vertices</vertex-name>
-* <edge-name>arangodb-rexter-graph-edges</edge-name>
-* <host>localhost</host>
-* <port>8529</port>
-* </properties>
-* </graph>
-* </code>
-*
-*
-* @author Achim Brandt (http://www.triagens.de)
-*/
+ * Rexster configuration for ArangoDB graphs. Accepts configuration in
+ * rexster.xml as follows:
+ * 
+ * <code>
+ *  <graph>
+ *     <graph-name>arangodb</graph-name>
+ *     <graph-type>com.tinkerpop.blueprints.impls.arangodb.utils.ArangoDBConfiguration</graph-type>
+ *     <properties>
+ *       <graph-name>arangodb-rexster-graph</graph-name>
+ *       <vertex-name>arangodb-rexster-graph-vertices</vertex-name>
+ *       <edge-name>arangodb-rexster-graph-edges</edge-name>
+ *       <host>localhost</host>
+ *       <port>8529</port>
+ *     </properties>
+ *     <extensions>
+ *       <allows>
+ *         <allow>tp:gremlin</allow>
+ *       </allows>
+ *     </extensions>
+ *   </graph>
+ * </code>
+ * 
+ * 
+ * @author Achim Brandt (http://www.triagens.de)
+ */
 
 public class ArangoDBConfiguration implements GraphConfiguration {
 
 	/**
-	 * Creates a ArangoDBGraph by a Rexster configuration
+	 * Creates a ArangoDBGraph by a Rexster configuration context
 	 * 
-	 * @param   configuration        Rexster configuration
-	 * @return  Graph                a ArangoDBGraph
+	 * @param context
+	 *            Rexster configuration context
+	 * @return Graph a ArangoDBGraph
 	 */
-		
-    public Graph configureGraphInstance(Configuration configuration) throws GraphConfigurationException {
-    	
-        final HierarchicalConfiguration graphSectionConfig = (HierarchicalConfiguration) configuration;
-        SubnodeConfiguration conf;
+	public Graph configureGraphInstance(GraphConfigurationContext context) throws GraphConfigurationException {
 
-        try {
-        	conf = graphSectionConfig.configurationAt(Tokens.REXSTER_GRAPH_PROPERTIES);
-        } catch (IllegalArgumentException iae) {
-            throw new GraphConfigurationException("Check graph configuration. Missing or empty configuration element: " + Tokens.REXSTER_GRAPH_PROPERTIES);
-        }
+		final HierarchicalConfiguration graphSectionConfig = (HierarchicalConfiguration) context.getProperties();
+		SubnodeConfiguration conf;
 
-        try {
+		try {
+			conf = graphSectionConfig.configurationAt(Tokens.REXSTER_GRAPH_PROPERTIES);
+		} catch (IllegalArgumentException iae) {
+			throw new GraphConfigurationException("Check graph configuration. Missing or empty configuration element: "
+					+ Tokens.REXSTER_GRAPH_PROPERTIES);
+		}
 
-            final String host = conf.getString("host");
-            final int port = conf.getInt("port", 5000);
-            final String graphName = conf.getString("graph-name");
-            final String vertexCollectionName = conf.getString("vertex-name");
-            final String edgeCollectionName = conf.getString("edge-name");
+		try {
 
-            return new ArangoDBGraph(host, port, graphName, vertexCollectionName, edgeCollectionName);
+			final String host = conf.getString("host");
+			final int port = conf.getInt("port", 5000);
+			final String graphName = conf.getString("graph-name");
+			final String vertexCollectionName = conf.getString("vertex-name");
+			final String edgeCollectionName = conf.getString("edge-name");
 
-        } catch (Exception ex) {
-            throw new GraphConfigurationException(ex);
-        }
-    }
-    
+			return new ArangoDBGraph(host, port, graphName, vertexCollectionName, edgeCollectionName);
+
+		} catch (Exception ex) {
+			throw new GraphConfigurationException(ex);
+		}
+	}
+
 }
